1f69629f6b8967a793c1bacd2c9b0bcc10823af7
Travis Pence <travis.pence@gmail.com>
2018-09-10 16:02:40 -0600

    Update paths for component views (attempt 1)

--- app/components/controllers/treeList.js
+++ app/components/controllers/treeList.js
@@ -7,7 +7,7 @@ var dataURL = "/resources/data/trees.json";
 angular.module('incuencaecuador.trees', [])
 	.component('treeList', {
 		bindings: {},
-		templateUrl: 'components/views/treeList.html',
+		templateUrl: 'app/components/views/treeList.html',
 		controller: function ($scope, $http) {
 			var $ctrl = this;
 			$ctrl.$onInit = function () {
@@ -22,7 +22,7 @@ angular.module('incuencaecuador.trees', [])
 	})
 	.component('treeDetail', {
 		bindings: {},
-		templateUrl: 'components/views/treeView.html',
+		templateUrl: 'app/components/views/treeView.html',
 		controller: function ($stateParams, $scope, $http) {
 			var $ctrl = this;
 			$ctrl.$onInit = function () {
